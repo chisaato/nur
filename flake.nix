fd9b891c18b5a03b07054a53d5ef7b16b0f627f2
chisaato <catsreporter@proton.me>
2025-08-21 06:15:09 +0800

    fix: 添加一些注释让自己明白

--- flake.nix
+++ flake.nix
@@ -7,13 +7,16 @@
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     {
+      # 传统包导出
       legacyPackages = forAllSystems (
         system:
         import ./default.nix {
           pkgs = import nixpkgs { inherit system; };
         }
       );
+      # Overlays 导出
       overlays.default = import ./overlay.nix;
+      # 标准 packages 导出
       packages = forAllSystems (
         system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
       );
