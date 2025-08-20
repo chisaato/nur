5e0f81a825c986ce1fe162adbc2737168afe6a14
chisaato <catsreporter@proton.me>
2025-08-21 04:21:33 +0800

    feat: add Garden CLI

--- default.nix
+++ default.nix
@@ -6,7 +6,9 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 {
   # The `lib`, `modules`, and `overlays` names are special
@@ -14,7 +16,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  garden-bin = pkgs.callPackage ./pkgs/garden-bin { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
---
+++ pkgs/garden-bin/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, fetchurl,lib }:
+
+stdenv.mkDerivation rec {
+  name = "garden-bin";
+  version = "0.14.7";
+  src = fetchurl {
+    url = "https://github.com/garden-io/garden/releases/download/${version}/garden-${version}-linux-amd64.tar.gz";
+    sha256 = "113cach2srja2x6gqxv8jcn57jxsdzwwjmhzn0nhcavl95m715p5";
+  };
+  sourceRoot = ".";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp linux-amd64/garden $out/bin/
+    chmod +x $out/bin/garden
+  '';
+
+  meta = with lib; {
+    description = "Garden Automation for Kubernetes development and testing.";
+    homepage = "https://github.com/garden-io/garden";
+    license = licenses.mpl20;
+    platforms = platforms.linux;
+  };
+}
